2abb7511c33f53f7f0cf46413dc6a787fe3f822a
Abraham Aviles <aaaviles1995@gmail.com>
2021-02-10 13:04:18 -0500

    Done

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,14 @@
 class Episode < ApplicationRecord
     has_many :appearances
     has_many :guest, through: :appearances
+
+    def average_rating
+        temp = 0.0
+        self.appearances.each do |a|
+            temp += a.rating
+        end
+        ans = temp/self.appearances.size
+        return ans
+    end
+
 end
