7f96f6e5edfc6b95e6c7dba5923bde394f6a3abe
Jacob Hampton <jhampton792@gmail.com>
2021-02-10 12:26:01 -0600

    Added advanced Appearance validation

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,4 +1,7 @@
 class Appearance < ApplicationRecord
     belongs_to :guest
     belongs_to :episode
+
+    validates :guest_id, uniqueness: { scope: :episode_id, message: ": It appears there's already a rating for this guest's appearance on the this episode."}
+    validates :rating, { inclusion: { in: [1, 2, 3, 4, 5], message: "must be an integer with a min of 1 and max of 5." } }
 end
