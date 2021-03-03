93da835cdee64ad972d2d408dc46a0fdf725fd9b
Jade McDaniels <jademcdaniels81@gmail.com>
2021-03-03 13:08:43 -0500

    submisison for deadline

--- Flatagram/src/index.js
+++ Flatagram/src/index.js
@@ -1,1 +1,26 @@
 // write your code here
+const IMG_URL = 'http://localhost:3000/images/1'
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetch(IMG_URL)
+        .then(resp => resp.json())
+        .then(postData => renderPost(postData))
+})
+
+
+function renderPost(postData){
+
+    for(images in postData){
+        console.log(`${title}: ${image.title}`)
+    }
+
+    // postData.images.forEach( images => {
+    //     let imageTitle = document.getElementsByClassName('title')
+    //         imageTitle.innerText = images.title
+    // }); *NOT AN ARRAY*
+    
+    // const imageTitle = document.getElementsByClassName('title')
+    //     imageTitle.innerHTML = `${images.title}`
+
+}   
+    
