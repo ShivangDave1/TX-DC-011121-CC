6d2d2dec8b9d43145c33fb4c6e1ed4a4196e373a
John Fuquene <jfuquene@email.wm.edu>
2021-03-24 12:59:39 -0400

    submission for deadline

--- Bot-Battlr/src/containers/BotsPage.js
+++ Bot-Battlr/src/containers/BotsPage.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy"
 
+
+const URL = 'http://localhost:6001/bots'
+const BOT_URL = 'http://localhost:6001/bots/'
 class BotsPage extends Component {
-  //start here with your code for step one
+ 
+  constructor(){
+    super()
+    this.state = {
+      bots: [],
+      army: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(res => res.json())
+    .then(bots => this.setState({bots}))
+  }
+
+  addBot = (bot) => {
+    if(!this.state.army.includes(bot)){
+    this.setState({army: [...this.state.army, bot]})
+  }
+  }
+  removeBot = (bot) => {
+    let newBots = this.state.army.filter(armyBot => armyBot !== bot)
+    this.setState({army: newBots})
+  }
+
+  destroyBot = (bot) => {
+     let newBots = this.state.bots.filter(Bot => Bot !== bot)
+    fetch(BOT_URL+bot.id, {method: "DELETE"})
+    .then(() => this.setState({bots: newBots}));
+    
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    
+    
+    return <div>
+      <YourBotArmy bots={this.state.army} removeBot={this.removeBot} destroyBot={this.destroyBot}/>
+      <BotCollection bots={this.state.bots} addBot={this.addBot} destroyBot={this.destroyBot}/>
+      
+      
+      {/* put your components here */}
+    
+    
+    </div>;
   }
 }
 
