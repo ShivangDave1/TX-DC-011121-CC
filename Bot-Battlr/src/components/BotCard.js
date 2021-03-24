ff2231fd966e3e499651cf340e71b9848bd00885
Joy Evans <joyevans@me.com>
2021-03-24 12:05:47 -0500

    submission for deadline

--- Bot-Battlr/src/components/BotCard.js
+++ Bot-Battlr/src/components/BotCard.js
@@ -9,46 +9,57 @@ const botTypeClasses = {
   Captain: "icon star"
 };
 
-const BotCard = props => {
+const BotCard = ({bot, clickAction, deleteAction}) => {
+// const BotCard = props => {
   return (
     <div className="ui column">
       <div
         className="ui card"
-        key={props.bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        key={bot.id}
+        // key={props.bot.id}
+        onClick={() => clickAction(bot)}
+        // onClick={() => clickAction(props)}
+        // onClick={() => console.log(props)}
       >
         <div className="image">
-          <img alt="oh no!" src={props.bot.avatar_url} />
+          <img alt="oh no!" src={bot.avatar_url} />
+          {/* <img alt="oh no!" src={props.bot.avatar_url} /> */}
         </div>
         <div className="content">
           <div className="header">
-            {props.bot.name}
-            <i className={botTypeClasses[props.bot.bot_class]} />
+            {bot.name}
+            {/* {props.bot.name} */}
+            <i className={botTypeClasses[bot.bot_class]} />
+            {/* <i className={botTypeClasses[props.bot.bot_class]} /> */}
           </div>
           <div className="meta text-wrap">
-            <small>{props.bot.catchphrase}</small>
+            <small>{bot.catchphrase}</small>
+            {/* <small>{props.bot.catchphrase}</small> */}
           </div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat" />
-            {props.bot.health}
+            {bot.health}
+            {/* {props.bot.health} */}
           </span>
 
           <span>
             <i className="icon lightning" />
-            {props.bot.damage}
+            {bot.damage}
+            {/* {props.bot.damage} */}
           </span>
           <span>
             <i className="icon shield" />
-            {props.bot.armor}
+            {bot.armor}
+            {/* {props.bot.armor} */}
           </span>
           <span>
             <div className="ui center aligned segment basic">
               <button
                 className="ui mini red button"
-                onClick={() =>
-                  console.log("add code to connect event listener")
+                onClick={() => deleteAction(bot)
+                  // console.log("add code to connect event listener")
                 }
               >
                 x
--- Bot-Battlr/src/containers/BotsPage.js
+++ Bot-Battlr/src/containers/BotsPage.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+
+const URL = 'http://localhost:6001/bots'
 
 class BotsPage extends Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    botArmy: []
+  }
+
+  addBot = (bot) => {
+    if(!this.state.botArmy.includes(bot)){
+    this.setState({botArmy: [...this.state.botArmy, bot]})
+    }
+  }
+
+  removeBot = (bot) => {
+    let updatedArmy = this.state.botArmy.filter(card => card !== bot)
+    this.setState({botArmy: updatedArmy})
+  }
+
+  deleteCard = (bot) => {
+    this.setState({
+      bots: this.state.bots.filter(card => card !== bot)
+    })
+  }
+
+  componentDidMount(){
+    fetch(URL)
+      .then(r => r.json())
+      .then(bots => this.setState({bots}))
+      // .then(bots => console.log(bots))
+  }
+
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+      <YourBotArmy deleteCard={this.deleteCard} removeBot={this.removeBot} bots={this.state.botArmy}/>
+      <BotCollection addBot={this.addBot} bots={this.state.bots}/>
+    </div>;
   }
 }
 
